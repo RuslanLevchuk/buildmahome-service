af326a0c81b3d053d5104bb7d18070c335b72728
RuslanLevchuk <rusik.levchuk.m@gmail.com>
2024-07-19 01:51:39 +0300

    added 'widget_tweaks' plugin to installed apps, added redirect routes after logining and logouting

--- buildmahome_service/settings.py
+++ buildmahome_service/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'buildmahome',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
