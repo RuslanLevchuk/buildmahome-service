8adf87be8d413adab1f2e845ec2ebf1440db97c8
RuslanLevchuk <rusik.levchuk.m@gmail.com>
2024-07-19 03:16:01 +0300

    added Form model SignUpForm for user creation, crated url route for user creation template, created UserCreateView in views.py

--- buildmahome/urls.py
+++ buildmahome/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from .views import (
-    IndexView
+    IndexView, UserCreateView
 )
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("accounts/sign_up/", UserCreateView.as_view(), name="sign_up"),
 ]
 
 app_name = "buildmahome"
--- buildmahome/views.py
+++ buildmahome/views.py
@@ -1,6 +1,17 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views import generic
+
+from buildmahome.forms import SignUpForm
 
 
-class IndexView(TemplateView):
+class IndexView(generic.TemplateView):
     template_name = "buildmahome/index.html"
+
+
+class UserCreateView(generic.CreateView):
+    template_name = "buildmahome/sign-up.html"
+    form_class = SignUpForm
+    success_url = reverse_lazy("buildmahome:index")
+
