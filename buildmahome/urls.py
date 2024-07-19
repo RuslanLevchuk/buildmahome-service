0d4df46f80767f9d0c9cc7ec8cbad2d4afeeb952
RuslanLevchuk <rusik.levchuk.m@gmail.com>
2024-07-20 02:48:20 +0300

    added user-datail template user-profile.html with related includes. Added UserDatailView in views.py with ability to get context about related to User Worker model, added route for UserDeatailView in urls.py

--- buildmahome/urls.py
+++ buildmahome/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from .views import (
-    IndexView, UserCreateView
+    IndexView, UserCreateView, UserProfileView
 )
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("accounts/sign_up/", UserCreateView.as_view(), name="sign_up"),
+    path("accounts/<int:pk>/profile/", UserProfileView.as_view(), name="profile"),
 ]
 
 app_name = "buildmahome"
--- buildmahome/views.py
+++ buildmahome/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.http import request
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
 from buildmahome.forms import SignUpForm
+from buildmahome.models import User, Worker
 
 
 class IndexView(generic.TemplateView):
@@ -11,7 +13,27 @@ class IndexView(generic.TemplateView):
 
 
 class UserCreateView(generic.CreateView):
-    template_name = "buildmahome/sign-up.html"
+    template_name = "registration/sign-up.html"
     form_class = SignUpForm
     success_url = reverse_lazy("buildmahome:index")
+    
+    
+class UserProfileView(generic.DetailView):
+    model = User
+    queryset = User.objects.all()
+    template_name = "buildmahome/user-profile.html"
+    context_object_name = "user"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.get_object()
+        try:
+            worker = user.worker
+        except Worker.DoesNotExist:
+            worker = None
+
+        context['worker'] = worker
+
+        if worker:
+            context['skills'] = worker.skills.all()
+        return context
